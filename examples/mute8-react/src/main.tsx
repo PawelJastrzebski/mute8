9e283e78baba6bd377b66cf2065b9459bf201528
Paweł Jastrzębski <paweljastrzebski96@gmail.com>
2024-01-30 08:41:50 +0100

    updated react example

--- examples/mute8-react/src/main.tsx
+++ examples/mute8-react/src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import CarStore from './CarStore.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <CarStore />
   </React.StrictMode>,
 )
