abe0eb07c702e8259f45412213a199e60f97edbb
Paweł Jastrzębski <paweljastrzebski96@gmail.com>
2024-01-31 09:14:03 +0100

    updated test

--- tests/mute8-react-render.spec.tsx
+++ tests/mute8-react-render.spec.tsx
@@ -6,24 +6,28 @@ import React from "react";
 import { render, renderWait } from "./utils"
 import { newState } from "../packages/mute8-react/mute8-react"
 
-const store = newState({
-    value: {
-        counter: 1
+test('render simple counter', async () => {
+    const store = newState({
+        value: {
+            counter: 1
+        }
+    })
+    
+    function TestComponent() {
+        const [count,] = store.useOne('counter')
+        return (<div id="count-value">{count}</div>)
     }
-})
 
-function TestComponent() {
-    const [count,] = store.useOne('counter')
-    return (<div id="count-value">{count}</div>)
-}
-
-test('render by js-dom', async () => {
+    // render
     const root = await render(<TestComponent />)
     const getText = () => root.querySelector("#count-value")?.innerHTML
-
     expect(getText()).toEqual("1")
+    // increment
     store.mut(v => v.counter++)
     await renderWait()
     expect(getText()).toEqual("2")
-  
+    // add 10
+    store.counter = store.counter + 10
+    await renderWait()
+    expect(getText()).toEqual("12")
 });
