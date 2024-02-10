f17e0abaca77cb482b99f7a1a28df17779515765
Paweł Jastrzębski <paweljastrzebski96@gmail.com>
2024-02-10 19:15:12 +0100

    mute8-solid updates

--- packages/mute8-solid/mute8-solid.ts
+++ packages/mute8-solid/mute8-solid.ts
@@ -1,10 +1,9 @@
-import * as mute8 from "../mute8/mute8"
-import { Store as StoreMute8, StoreDefiniton, ProxyExtension, Plugin } from "../mute8/mute8"
-import { createSignal, createEffect } from 'solid-js';
+import { Store as StoreMute8, StoreDefiniton, ProxyExtension, newStoreProxy } from "../mute8/mute8"
+import { createSignal, Accessor, onCleanup, } from 'solid-js';
 
 interface SolidExtension<T> {
-    use(): [() => T, (newValeu: Partial<T>) => void]
-    useOne<K extends keyof T>(property: K): [() => T[K], (newValue: T[K]) => void]
+    use(): [Accessor<T>, (newValeu: Partial<T>) => void]
+    useOne<K extends keyof T>(property: K): [Accessor<T[K]>, (newValue: T[K]) => void]
 }
 
 export type Store<T, A, AA> = StoreMute8<T, A, AA> & {
@@ -18,25 +17,21 @@ export const newStore = <T extends Object, A, AA>(store: StoreDefiniton<T, A, AA
             return {
                 use() {
                     const [value, setValue] = createSignal(core.s());
-                    createEffect(() => {
-                        const sub = core.sub(s => setValue(s as any))
-                        return () => sub.destroy()
-                    }, [value])
+                    const sub = core.sub(s => setValue(s as any))
+                    onCleanup(() => sub.destroy())
                     return [value, (v: any) => core.u(v)]
                 },
                 useOne(property: keyof T) {
                     const [value, setValue] = createSignal((core.s() as any)[property]);
-                    createEffect(() => {
-                        const sub = core.sub((s: any) => setValue(s[property]))
-                        return () => sub.destroy()
-                    }, [value])
+                    const sub = core.sub((s: any) => setValue(s[property]))
+                    onCleanup(() => sub.destroy())
                     return [value, (v: any) => core.uv(property, v)]
                 }
             }
         },
     }
 
-    return mute8.newStoreProxy(store as any, reactExtension) as Store<T, A, AA>
+    return newStoreProxy(store as any, reactExtension) as Store<T, A, AA>
 }
 
 export { SubFn, VoidFn, AsyncFn, Sub, Plugin } from "../mute8/mute8"
