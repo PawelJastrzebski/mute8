07a65c8181e35d25eef7f25bea9e2884fecb3196
Paweł Jastrzębski <paweljastrzebski96@gmail.com>
2024-02-17 12:31:59 +0100

    added mute8-vue

---
+++ examples/mute8-vue/src/examples/Counter.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { counter, selectName } from '../services/counter';
+defineProps<{ msg: string }>()
+const count = counter.vue.useOne("count")
+const name = selectName()
+</script>
+
+<template>
+  <h1>{{ msg == "" ? "Empty Name" : msg }}</h1>
+  <div class="card">
+    <button type="button" @click="counter.count = counter.count + 1">count is {{ count }}</button>
+    <div>App Name (direct update) <br />
+      <input :value="name" @input="event => counter.name = (event as any).target.value">
+    </div>
+    <div>App Name (action) <br />
+      <input :value="name" @input="event => counter.actions.setName((event as any).target.value)">
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ examples/mute8-vue/src/main.ts
@@ -0,0 +1,7 @@
+import "https://paweljastrzebski.github.io/mute8/devtools/v1.mjs"
+import './style.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#root')
